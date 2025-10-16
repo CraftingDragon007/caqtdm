794a61feabbc682cdcb3480ad2e332fc804d65e5
CraftingDragon007 <craftingdragon007@outlook.com>
2025-10-16 15:16:09 +0200

    Renamed documentation to index for better compatibility with github pages, added LaTeX to the pages publishing action to be able to render math

--- caQtDM_docs/source/conf.py
+++ caQtDM_docs/source/conf.py
@@ -10,7 +10,7 @@ project = "caQtDM"
 copyright = "2012-2025, Anton Mezger, Helge Brands"  # GPL3
 author = "Anton Mezger, Helge Brands"
 release = "4.5.0"
-root_doc = "documentation"
+root_doc = "index"
 
 
 # -- General configuration ---------------------------------------------------
