6d65284e6103bfc98991baabae5e8cde7c9c2652
CraftingDragon007 <info@craftingdragon.ch>
2025-11-28 14:17:52 +0100

    Added novnc_host option to allow listening on a different host with the novnc plugin

--- caQtDM_Viewer/src/caQtDM.cpp
+++ caQtDM_Viewer/src/caQtDM.cpp
@@ -220,6 +220,7 @@ int main(int argc, char *argv[])
     bool use_novnc_plugin = false;
     bool slave_server = false;
     bool web_interaction_based_timeout = false;
+    QString host = "localhost";
     quint16 port = 5900;
     quint16 web_port = port + 1000;
     uint web_timeout = 0;
@@ -355,6 +356,9 @@ int main(int argc, char *argv[])
             }
         } else if (strcmp (argv[in], "-web_interaction_timeout") == 0) {
             web_interaction_based_timeout = true;
+        } else if (strcmp (argv[in], "-novnc_host")) {
+            in++;
+            host = QString(argv[in]);
         } else if (strncmp (argv[in], "-" , 1) == 0) {
             /* unknown application argument */
             printf("caQtDM -- Argument %d = [%s] is unknown!, possible -attach -macro -noMsg -stylefile -dg -x -print -httpconfig -noResize -option\n",in,argv[in]);
@@ -371,7 +375,7 @@ int main(int argc, char *argv[])
             if (dimensions.height <= 0 || dimensions.width <= 0) {
                 printf("caQtDM -- Negative or zero ui width / height found (%sx%s), not enabling server mode!", QString::number(dimensions.width).toUtf8().data(), QString::number(dimensions.height).toUtf8().data());
                 server = false;
-            } else qputenv("QT_QPA_PLATFORM", ("%1:size=" + QString::number(dimensions.width) + "x" + QString::number(dimensions.height) + ":depth=16:port=" + QString::number(port)).arg(use_novnc_plugin ? "novnc" : "vnc").toUtf8());
+            } else qputenv("QT_QPA_PLATFORM", ("%1:size=" + QString::number(dimensions.width) + "x" + QString::number(dimensions.height) + ":depth=16:port=" + QString::number(port) + (use_novnc_plugin ? ":host=" + host : "")).arg(use_novnc_plugin ? "novnc" : "vnc").toUtf8());
         }
     } else if (server) {
         printf("No ui file was specified, not enabling server mode!");
