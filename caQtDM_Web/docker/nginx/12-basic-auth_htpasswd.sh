f5af6e59c7f2dcc17847ad44eeceb585942d8c79
CraftingDragon007 <info@craftingdragon.ch>
2026-01-13 13:58:19 +0100

    Added basic user password authorization to nginx docker

---
+++ caQtDM_Web/docker/nginx/12-basic-auth_htpasswd.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ -n "$BASIC_AUTH_USER" ] && [ -n "$BASIC_AUTH_PASSWORD" ]; then
+    echo "Creating .htpasswd file for user: $BASIC_AUTH_USER"
+    
+    htpasswd -cb -B /etc/nginx/.htpasswd "$BASIC_AUTH_USER" "$BASIC_AUTH_PASSWORD"
+    
+    echo "Basic authentication has been configured."
+else
+    echo "BASIC_AUTH_USER or BASIC_AUTH_PASSWORD not set. Skipping Basic Auth setup."
+fi
