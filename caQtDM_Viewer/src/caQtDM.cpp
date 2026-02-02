a00c9631e5d0f658f9bd05a06735e6d94ee9c672
CraftingDragon007 <info@craftingdragon.ch>
2026-02-02 15:46:29 +0100

    Removed wrong indents, removed empty else preprocessor statement

--- caQtDM_Viewer/src/caQtDM.cpp
+++ caQtDM_Viewer/src/caQtDM.cpp
@@ -479,8 +479,6 @@ int main(int argc, char *argv[])
 #if QT_VERSION < QT_VERSION_CHECK(6,0,0)
         // fix dpi for (no)VNC with qt5
         qputenv("QT_FONT_DPI", QString::number(96).toUtf8());
-#else
-
 #endif
     }
 
