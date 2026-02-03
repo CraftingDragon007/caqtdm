600abddfe2d85abc14dd67ccbdd4d3e652f52c57
CraftingDragon007 <info@craftingdragon.ch>
2026-02-03 16:39:13 +0100

    Added some sanity checks to hmisharedeventbus.cpp

--- caQtDM_Lib/src/hmisharedeventbus.cpp
+++ caQtDM_Lib/src/hmisharedeventbus.cpp
@@ -176,10 +176,16 @@ bool HmiSharedEventBus::sendEvent(int eventType, const QByteArray& payload) {
         return false;
     }
 
-    this_writeLockSemaphore.acquire();
+    if (!this_writeLockSemaphore.acquire()) {
+        qCritical() << PREFIX << "Unable to aquire system semaphore, error:" << this_writeLockSemaphore.errorString();
+        qCritical() << PREFIX << "To prevent crashes HmiShharedEventBus is now disabled!";
+        qCritical() << PREFIX << "Please clean up /tmp/*caQtDM* and restart the program if you want to use caHMIConfig features";
+        this_isInitialized = false;
+        this_pollTimer.stop();
+        return false;
+    }
 
-    int writeIndex = this_header->currentWriteIndex;
-    int nextWriteIndex = (writeIndex + 1) % EVENT_BUFFER_CAPACITY;
+    int writeIndex = this_header->currentWriteIndex % EVENT_BUFFER_CAPACITY;
 
     EventPayload& event = this_eventBuffer[writeIndex];
     event.eventType = eventType;
@@ -191,6 +197,8 @@ bool HmiSharedEventBus::sendEvent(int eventType, const QByteArray& payload) {
     } else {
         std::memset(event.data, 0, EVENT_PAYLOAD_SIZE); // Clear if no payload.
     }
+
+    int nextWriteIndex = (writeIndex + 1) % EVENT_BUFFER_CAPACITY;
 
     this_header->currentWriteIndex = nextWriteIndex;
     this_header->totalEventsWritten++;
