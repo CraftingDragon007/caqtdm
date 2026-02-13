f0d70bbba007d1e99b3db82895ebd7decba969d3
CraftingDragon007 <info@craftingdragon.ch>
2026-02-13 09:50:40 +0100

    Removed left over causer id export

--- caQtDM_Lib/src/causerid.h
+++ caQtDM_Lib/src/causerid.h
@@ -1,9 +1,8 @@
 #ifndef CAUSERID_H
 #define CAUSERID_H
 
-#include "caQtDM_Lib_global.h"
 #include <QString>
 
-CAQTDM_LIBSHARED_EXPORT QString getUniqueUserId();
+QString getUniqueUserId();
 
 #endif // USERID_H
