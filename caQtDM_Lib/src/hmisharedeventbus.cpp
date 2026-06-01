4a8096a2d0bb272616f9505e1e974ce1abfd60a4
Helge Brands <helge.brands@psi.ch>
2026-06-01 13:27:49 +0200

    next bug in hmi shared memory, try to fix.

--- caQtDM_Lib/src/hmisharedeventbus.cpp
+++ caQtDM_Lib/src/hmisharedeventbus.cpp
@@ -188,7 +188,11 @@ bool HmiSharedEventBus::sendEvent(int eventType, const QByteArray& payload) {
     }
 
     quint32 writeIndex = this_header->currentWriteIndex % EVENT_BUFFER_CAPACITY;
-
+    if (!this_eventBuffer) {
+        qCritical() << PREFIX << "this_eventBuffer is a null pointer.";
+        this_writeLockSemaphore.release();
+        return false;
+    }
     EventPayload& event = this_eventBuffer[writeIndex];
     event.eventType = eventType;
     event.senderPid = QCoreApplication::applicationPid();
--- caQtDM_Viewer/package/rhel/caqtdm.spec
+++ caQtDM_Viewer/package/rhel/caqtdm.spec
@@ -29,7 +29,7 @@
 Name:    caqtdm 
 Summary: Qt Widgets for Technical Applications
 Version: 4.6.1
-Release: 1.0%{?dist}
+Release: 1.2%{?dist}
 #############################################################################
 License: GPLv3
 URL:     https://github.com/caqtdm/caqtdm
