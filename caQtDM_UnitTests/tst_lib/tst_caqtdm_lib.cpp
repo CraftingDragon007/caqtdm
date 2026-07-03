293e846b480c749adf636fd5527dab8328dc68d3
Helge Brands <helge.brands@psi.ch>
2026-07-03 11:53:05 +0200

    adding the manual macrotests.ui data to the test

--- caQtDM_UnitTests/tst_lib/tst_caqtdm_lib.cpp
+++ caQtDM_UnitTests/tst_lib/tst_caqtdm_lib.cpp
@@ -263,4 +263,80 @@ void TestCaQtDM_Lib::treatMacroWorks()
         QCOMPARE(m_caQtDM_Lib->treatMacro(map, text, &doNothing, "widget"), QString("pv $(A)"));
         QCOMPARE(doNothing, true);
     }
+
+    m_caQtDM_Lib->unknownMacrosList.clear();
+
+    {
+        // Cases taken from the manual display test caQtDM_Tests/macrotest.ui,
+        // which pairs each expression with its expected result for the macros
+        // TEST=12345, TEST1=abcd, TESTLEER=<empty>; TEST2..TEST5 are undefined.
+        QMap<QString, QString> map;
+        map["TEST"] = "12345";
+        map["TEST1"] = "abcd";
+        map["TESTLEER"] = "";
+
+        const struct {
+            const char *input;
+            const char *expected;
+        } cases[] = {
+            {R"lim($(TEST))lim", "12345"},
+            {R"lim($(TEST)$(TEST1)$(TEST)$(TEST1)$(TEST)$(TEST1))lim",
+             "12345abcd12345abcd12345abcd"},
+            {R"lim(xyz$$876_$(TEST{"regex":"([1,2]+)([3])","value":"A\\1"})______)lim",
+             "xyz$$876_A1245______"},
+            {R"lim($(TEST{Bla})$(TEST1)$(TEST))lim", "12345abcd12345"},
+            {R"lim($(TEST{"regex":"\\S+","value":"HUI2HUIHUI"}) $(TEST{Bla}) $(TEST1)$(TEST))lim",
+             "HUI2HUIHUI 12345 abcd12345"},
+            {R"lim($(TEST{"regex":"([1,2]+)([3])","value":"A"}))lim", "A45"},
+            {R"lim($(TEST{"regex":"([1,2]+)([3])","value":"A\\2"}))lim", "A345"},
+            {R"lim($(TEST5=This) $(TEST5=is) $(TEST5=a) $(TEST5=macro) $(TEST5=test))lim",
+             "This is a macro test"},
+            {R"lim( $(TEST=Error!!))lim", " 12345"},
+            {R"lim($(TEST5{"regex":"\\S+","value":"$(TEST1)"}=RegExConst))lim", "RegExConst"},
+            {R"lim($(TEST5{"regex":"\\S+","value":"$(TEST1)"}=$(TEST)))lim", "12345"},
+            {R"lim($(TEST5=$(TEST4=$(TEST3=chain constant test))))lim", "chain constant test"},
+            {R"lim($(TEST5=$(TEST4=$(TEST3=$(TEST1)))))lim", "abcd"},
+            {R"lim($(TEST5=->$(TEST=$(TEST3=$(TEST4)))<-))lim", "->12345<-"},
+            {R"lim($(TESTLEER=1$(TEST=2$(TEST3=3$(TEST4)3)2)1))lim", ""},
+            {R"lim($(TESTLEER)<-LEER)lim", "<-LEER"},
+            {R"lim(->$(TESTLEER=not OK)$(TEST=$(TEST3=Boing))$(TEST4=)<-)lim", "->12345<-"},
+            {R"lim($(TESTLEER)-$(TEST))lim", "-12345"},
+            {R"lim($(TEST)-$(TESTLEER)$(TEST)=0)lim", "12345-12345=0"},
+            {R"lim(->$(TESTLEER=Error)<-)lim", "-><-"},
+            {R"lim($(TEST2=1);$(TEST2=2);$(TEST2=3);$(TEST2=4);$(TEST2=5);$(TEST2=6))lim",
+             "1;2;3;4;5;6"},
+        };
+
+        for (const auto &testCase : cases) {
+            bool doNothing = true;
+            QString result = m_caQtDM_Lib->treatMacro(map,
+                                                      QString::fromUtf8(testCase.input),
+                                                      &doNothing,
+                                                      "widget");
+            QVERIFY2(result == QString::fromUtf8(testCase.expected),
+                     qPrintable(QString("input '%1' gave '%2', expected '%3'")
+                                    .arg(testCase.input, result, testCase.expected)));
+            QCOMPARE(doNothing, false);
+        }
+        QCOMPARE(m_caQtDM_Lib->unknownMacrosList.isEmpty(), true);
+    }
+
+    m_caQtDM_Lib->unknownMacrosList.clear();
+
+    {
+        // also from macrotest.ui: an undefined macro without default value
+        // stays untouched and is registered as unknown
+        QMap<QString, QString> map;
+        map["TEST"] = "12345";
+        map["TEST1"] = "abcd";
+        map["TESTLEER"] = "";
+
+        QString text = R"lim($(TEST2),not Resolved Macro=OK)lim";
+        bool doNothing = true;
+
+        QCOMPARE(m_caQtDM_Lib->treatMacro(map, text, &doNothing, "widget42"), text);
+        QCOMPARE(doNothing, false);
+        QCOMPARE(m_caQtDM_Lib->unknownMacrosList.size(), 1);
+        QCOMPARE(m_caQtDM_Lib->unknownMacrosList.contains("(TEST2)###widget42###test.ui"), true);
+    }
 }
